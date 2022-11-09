3bfc3ba1ff39bdfb8e1fb29170e5da28ecb464a5
santy-wang <wh19930829@qq.com>
2022-11-09 20:06:00 +0800

    add transform system

---
+++ cocos/new-ui/subsystem/ui-transform-subsystem.ts
@@ -0,0 +1,19 @@
+import { InvalidateReason, UIElement } from "../base/ui-element";
+import { UISubSystem } from "../base/ui-subsystem";
+
+export class UITransformSubsystem extends UISubSystem {
+    private _dirtyTransformMap = new Set<UIElement>();
+
+    invalidate (element: UIElement, invalidateReason: InvalidateReason) {
+        if (invalidateReason & InvalidateReason.TRANSFORM) {
+            this._dirtyTransformMap.add(element);
+        }
+    }
+
+    update () {
+        for (const element of this._dirtyTransformMap) {
+            element.updateWorldTransform();
+        }
+        this._dirtyTransformMap.clear();
+    }
+}
